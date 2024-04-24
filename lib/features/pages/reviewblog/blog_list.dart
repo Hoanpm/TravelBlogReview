d2b56ea691cea228a375626c79837ffc62478f39
Hoanpm <hoan922003@gmail.com>
2024-04-24 23:04:30 +0700

    test branch

--- lib/features/pages/reviewblog/blog_list.dart
+++ lib/features/pages/reviewblog/blog_list.dart
@@ -10,7 +10,7 @@ class BlogList extends ConsumerWidget {
     return Column(
       children: [
         BlogCard(imageUrl: "https://gcs.tripi.vn/public-tripi/tripi-feed/img/474068pFP/100-hinh-nen-facebook-cute-de-thuong-dang-yeu-dep-nhat_013717067.jpg", textContent: "Bài đăng 001", time: "10:00 AM", like: 100, comment: 20, fullName: "Lương Đức", userName: "@Vduc69"),
-        BlogCard(imageUrl: "https://ca.slack-edge.com/T02QFU9TCTD-U064XJS8UGH-3b52a500b766-512", textContent: "Tôi mới đi chịch chó với chịch gay, hiện đang luyện bú cu tại đảo gay", time: "11:00 PM", like: 100000, comment: 20000, fullName: "Sun Slave", userName: "Vuachoigay"),
+        BlogCard(imageUrl: "https://ca.slack-edge.com/T02QFU9TCTD-U064XJS8UGH-3b52a500b766-512", textContent: "Tôi mới đi chịch chó với chịch gay, hiện đang luyện bú cu tại đảo gay và thành master gay", time: "11:00 PM", like: 100000, comment: 20000, fullName: "Sun Slave", userName: "Vuachoigay"),
         BlogCard(imageUrl: "https://gcs.tripi.vn/public-tripi/tripi-feed/img/474068Ehu/anh-bia-facebook-mau-hong-de-thuong_081148263.jpg", textContent: "Bài đăng 002", time: "01:PM", like: 1000, comment: 400, fullName: "Hiếu nguyễn", userName: "@hieuminh"),
         BlogCard(imageUrl: "", textContent: "", time: "", like: 100, comment: 1, fullName: "", userName: ""),
       ],
