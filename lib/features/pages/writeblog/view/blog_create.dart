3b872a44de90efd44fdd8649f66e4ad4896eb409
Hoanpm <hoan922003@gmail.com>
2024-05-16 23:17:24 +0700

    fix blogcreate

--- lib/features/pages/writeblog/view/blog_create.dart
+++ lib/features/pages/writeblog/view/blog_create.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travelblog/color/color.dart';
 import 'package:travelblog/features/auth/widget/auth_field.dart';
 import 'package:travelblog/features/pages/writeblog/widget/Dropdown_Component.dart';
 //import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -27,121 +28,140 @@ class _BlogCreateState extends State<BlogCreate> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        padding: EdgeInsets.only(top: 20),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  width: 170,
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(115, 115, 115, 1),
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Center(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.menu,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      CustomDropdown(items: travelTypes, hintText: "Thể loại"),
-                    ],
-                  )),
-                ),
-                Container(
-                  width: 170,
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(115, 115, 115, 1),
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Center(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.location_on,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      CustomDropdown(items: travelTypes, hintText: "Map"),
-                    ],
-                  )),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  width: 170,
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(115, 115, 115, 1),
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Center(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.location_city,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      CustomDropdown(
-                          items: travelTypes, hintText: "Tỉnh/Thành"),
-                    ],
-                  )),
-                ),
-                Container(
-                  width: 170,
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(115, 115, 115, 1),
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Center(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.location_city,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      CustomDropdown(
-                          items: travelTypes, hintText: "Quận/Huyện"),
-                    ],
-                  )),
-                )
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 20, top: 20),
-              child: Column(
-                children: [
-                  AuthField(controller: location, hintText: "Địa chỉ ..."),
-                  AuthField(controller: content, hintText: "Nội dung bài viết ..."),
-                ],
+      
+      child: Column(
+        children: [
+          SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 170,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(115, 115, 115, 1),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Center(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    CustomDropdown(items: travelTypes, hintText: "Thể loại"),
+                  ],
+                )),
               ),
-            )
-          ],
-          
-        ),
+              Container(
+                width: 170,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(115, 115, 115, 1),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Center(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    CustomDropdown(items: travelTypes, hintText: "Map"),
+                  ],
+                )),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 170,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(115, 115, 115, 1),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Center(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.location_city,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    CustomDropdown(
+                        items: travelTypes, hintText: "Tỉnh/Thành"),
+                  ],
+                )),
+              ),
+              Container(
+                width: 170,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(115, 115, 115, 1),
+                    borderRadius: BorderRadius.circular(20)),
+                child: Center(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.location_city,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    CustomDropdown(
+                        items: travelTypes, hintText: "Quận/Huyện"),
+                  ],
+                )),
+              )
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 30, top: 20,right: 30),
+            child: Column(
+              children: [
+                AuthField(controller: location, hintText: "Địa chỉ ...", ishiddenText: false,),
+                Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                      
+                      borderRadius: BorderRadius.circular(5)
+                    ),
+                  child: TextFormField(
+                    expands: true,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      hintText:
+                          "Nội dung bài viết ...",
+                      hintStyle: TextStyle(
+                          color: PJcolor.primaryColor2,
+                          fontSize: 20,
+                          fontFamily: "noto",
+                          fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.visible),
+                      border: InputBorder.none
+                    )
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
