3cbcb4d15c66b911f7b4aa3a4385f377b50144fd
Hoanpm <hoan922003@gmail.com>
2024-05-16 16:21:44 +0700

    hidden password

--- lib/features/auth/widget/auth_field.dart
+++ lib/features/auth/widget/auth_field.dart
@@ -4,34 +4,32 @@ import 'package:travelblog/color/color.dart';
 class AuthField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
-  const AuthField({
-    super.key, 
-    required this.controller,
-    required this.hintText,
-    });
+  final bool ishiddenText;
+  const AuthField(
+      {super.key, required this.controller, required this.hintText, required this.ishiddenText});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: ishiddenText,
       controller: controller,
       decoration: InputDecoration(
-        border: InputBorder.none,
-        // focusedBorder: OutlineInputBorder(
-        //   borderRadius: BorderRadius.circular(5),
-        //   borderSide: const BorderSide(
-        //     color: Colors.lightBlue,
-        //     width: 3
-        //   )
-        // ),
-        contentPadding: const EdgeInsets.fromLTRB(0, 18, 18, 18),
-        hintText: hintText, 
-        hintStyle: const TextStyle(
-          fontFamily: "noto",
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-          color: PJcolor.primaryColor2,
-        )
-      ),
+          border: InputBorder.none,
+          // focusedBorder: OutlineInputBorder(
+          //   borderRadius: BorderRadius.circular(5),
+          //   borderSide: const BorderSide(
+          //     color: Colors.lightBlue,
+          //     width: 3
+          //   )
+          // ),
+          contentPadding: const EdgeInsets.fromLTRB(0, 18, 18, 18),
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            fontFamily: "noto",
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: PJcolor.primaryColor2,
+          )),
     );
   }
 }
